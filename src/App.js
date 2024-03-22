92aefd25e1ba0bd7da1d212c3f0c2f8d1094864b
XurshidbekTV <xurshidbektursunaliyev@gmail.com>
2024-03-22 14:06:02 +0500

    chalenge

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+     <Avatar/>
+     <div className="data">
+      <Intro/>
+      <SkillList/>
+     </div>
     </div>
   );
 }
 
+function Avatar(){
+  return(
+  <img src="img/Untitled.png" alt="unteted" className='avatar'/>
+ )
+}
+function Intro(){
+  return(
+    <div>
+      <h1>Jonsn Schendment</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus quaerat voluptas delectus quidem similique dolore vel, velit sed, nemo nisi at aut necessitatibus nulla illum esse labore optio dicta laborum!</p>
+    </div>
+  )
+}
+function SkillList(){
+return(
+  <div className='skill-list'>
+    <Skill skill='React' emoj='👌' color='#123456'/>
+    <Skill skill='JavaScript' emoj='👌' color='pink'/>
+    <Skill skill='Html + Css' emoj='👌' color='orangered'/>
+    <Skill skill='Sevlet' emoj='👌' color='orange'/>
+    <Skill skill='Bootstrap' emoj='👌' color='yellow'/>
+  </div>
+)
+}
+function Skill(props){
+return(
+  <div className='skill' style={{backgroundColor: props.color}}>
+    <span >{props.skill}</span>
+    <span>{props.emoj}</span>
+  </div>
+)
+}
 export default App;
